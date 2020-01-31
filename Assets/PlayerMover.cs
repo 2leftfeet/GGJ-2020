e11948cc7a28b32230def997bbec7a5c99f162e2
unknown <liutauras.gaidamavicius@gmail.com>
2020-02-01 01:24:02 +0200

    Electro challenge

---
+++ Assets/PlayerMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    GravityObject gravityObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravityObject = GetComponent<GravityObject>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.up, -gravityObject.gravityDirection);
+    }
+}
