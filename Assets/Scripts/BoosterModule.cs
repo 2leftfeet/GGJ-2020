b676643a7426562294df82c695d178a6a868f837
Aivaras Ivoškus <aiva.ivoskus@gmail.com>
2020-02-01 14:52:35 +0200

    More weird stuff

--- Assets/Scripts/BoosterModule.cs
+++ Assets/Scripts/BoosterModule.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class BoosterModule : CarriableObject
 {
-    public override void OnEquip()
+    public override void OnEquip(CharacterHands characterHands)
     {
-        base.OnEquip();
+        base.OnEquip(characterHands);
+
+        characterHands.GetComponent<BaseCharacterController>().hasDoubleJump = true;
     }
 
-    public override void OnUnequip()
+    public override void OnUnequip(CharacterHands characterHands)
     {
-        base.OnUnequip();
+        base.OnUnequip(characterHands);
+
+        characterHands.GetComponent<BaseCharacterController>().hasDoubleJump = false;
     }
 }
