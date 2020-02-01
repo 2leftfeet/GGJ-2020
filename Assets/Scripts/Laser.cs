bd0a4c9c95a586c553a4f67778d15fc8b6bf2d5b
Justas Žilinskas <justasz@unity3d.com>
2020-02-01 13:31:55 +0200

    lazer trash

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    LineRenderer lr;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        lr.SetPosition(0, startPoint.position);
+        //lr.SetPosition(1, endPoint.position);
+
+        RaycastHit hit;
+        if (Physics.Raycast(startPoint.position, endPoint.position, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        else lr.SetPosition(1, endPoint.position);
+    }
+}
