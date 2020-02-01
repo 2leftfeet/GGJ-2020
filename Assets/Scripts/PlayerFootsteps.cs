37c16c1a3e71c2b95cac7bf54c424767928d8a90
Aivaras Ivoškus <aiva.ivoskus@gmail.com>
2020-02-01 22:14:20 +0200

    Commit voice

--- Assets/Scripts/PlayerFootsteps.cs
+++ Assets/Scripts/PlayerFootsteps.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PlayerFootsteps : MonoBehaviour
 {
     public AudioClip[] audioClips;
+    private AudioSource audioSource;
+    private Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(rigidbody.velocity.magnitude > 0.5f && Mathf.Abs(rigidbody.velocity.y) < 0.01f && !audioSource.isPlaying)
+        {
+            audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+            audioSource.Play();
+        }
     }
 }
