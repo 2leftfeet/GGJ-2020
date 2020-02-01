805cc08e7b6ebd65e2fac4bfa44457b146734072
Aivaras <aivaras.ivoskus@mif.stud.vu.lt>
2020-02-01 11:42:24 +0200

    WHole lot of changes from yesterday

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,8 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public void LoadLevel(int levelIndex)
+    public static LevelManager instance;
+
+    public void Awake()
     {
-        SceneManager.LoadScene(levelIndex);
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadLevelAdditive(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex, LoadSceneMode.Additive);
+    }
+
+    public void UnloadLevel(int levelIndex)
+    {
+        SceneManager.UnloadSceneAsync(levelIndex);
     }
 }
