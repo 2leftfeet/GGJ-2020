29a959852a95e5551ac0f47f4034d6a3858ee19d
unknown <liutauras.gaidamavicius@gmail.com>
2020-02-01 11:28:15 +0200

    added "always moving" to moving platforms

--- Assets/PuzzleScripts/PuzzleMovingPlatform.cs
+++ Assets/PuzzleScripts/PuzzleMovingPlatform.cs
@@ -8,6 +8,8 @@ public class PuzzleMovingPlatform : PuzzleMechanism
     [SerializeField] Vector3 activatedOffset;
     [SerializeField] float platformSpeed = 1.0f;
 
+    [SerializeField] bool alwaysActivated = false;
+
     Vector3 pos1;
     Vector3 pos2;
 
@@ -20,6 +22,7 @@ public class PuzzleMovingPlatform : PuzzleMechanism
     {
         pos1 = transform.position;
         pos2 = transform.position + activatedOffset;
+        if (alwaysActivated) moving = true;
     }
 
     public override void Activate()
@@ -29,7 +32,7 @@ public class PuzzleMovingPlatform : PuzzleMechanism
 
     public override void Deactivate()
     {
-        moving = false;
+        if(!alwaysActivated) moving = false;
     }
 
     // Update is called once per frame
