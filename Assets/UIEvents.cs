b4a3d1bf2953ca7f4a53955cd89fd1aea1a99c73
Rokas <rokas@kazlauskas.eu>
2020-02-02 09:47:18 +0200

    Crunch Done

--- Assets/UIEvents.cs
+++ Assets/UIEvents.cs
@@ -5,5 +5,35 @@ using TMPro;
 
 public class UIEvents : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI shipLeg;
 
+    [SerializeField]
+    private TextMeshProUGUI fuelCanister;
+
+    [SerializeField]
+    private TextMeshProUGUI energyCube;
+
+    [SerializeField]
+    private TextMeshProUGUI rocketBooster;
+
+    public void FoundShipLeg()
+    {
+        shipLeg.color = new Color32(0, 0, 0, 255);
+    }
+
+    public void FoundFuelCanister()
+    {
+        fuelCanister.color = new Color32(0, 0, 0, 255);
+    }
+
+    public void FoundPinkCube()
+    {
+        energyCube.color = new Color32(0, 0, 0, 255);
+    }
+
+    public void FoundRocketBoost()
+    {
+        rocketBooster.color = new Color32(0, 0, 0, 255);
+    }
 }
